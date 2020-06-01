45e5631db0d9f4267d42554ff55a646f43deeae5
goranp13 <goranp13@gmail.com>
2020-06-01 15:13:02 +0200

    eto hello opet

---
+++ go_proba/go_opet/001-Hello-world/001hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world, here we go again :-)!")
+	foo()
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo!!!")
+}
