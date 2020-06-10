08e9e145e00f370005f383dc199cd6abb5920c56
goranp13 <goranp13@gmail.com>
2020-06-10 14:54:59 +0200

    ninja l2 start

---
+++ go_proba/go_opet/Hands-on-exercise/2jedi1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 76
+	fmt.Printf("Decimal: %d\n", x)
+	fmt.Printf("Binary: %b\n", x)
+	fmt.Printf("Hexadecimal: %#x\n", x)
+
+}
---
+++ go_proba/go_opet/Hands-on-exercise/2jedi2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := (42 == 42)
+	b := (42 <= 42)
+	c := (42 >= 43)
+	d := (42 != 42)
+	e := (42 < 43)
+	f := (42 > 42)
+
+	fmt.Println(a, b, c, d, e, f)
+}
